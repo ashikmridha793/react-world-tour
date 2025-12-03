b633fee683c2af41a7245f43214717ab85b71a9c
Ashikmridha793 <ashikmridha793@gmail.com>
2025-12-04 00:13:27 +0600

    commit flage

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
-      const [countries, setCountries ] = useState([])
-      useEffect(()=> {
-            fetch('https://restcountries.com/v3.1/all')
-            .then(res => res.json())
-            .then(data => console.log(data))
-      }, [])
 
-      return (
-            <div>
-                  <h3>Countries:</h3>
-            </div>
-      );
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/independent?status=true")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+
+    <div>
+
+      <h3>Countries: {countries.length}</h3>
+
+      {
+            countries.map((country) => (
+            <Country key={country.cca3} country={country}></Country>
+            ))
+      }
+
+    </div>
+
+  );
 };
 
 export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,14 @@
+import './Country.css'
+const Country = ({ country }) => {
+      const {name, flags, population, area} = country
+      return (
+            <div className="country">
+                  <h3>{name.common}</h3>
+                  <img src={flags.png} alt="" />
+                  <p>Total Population: {population}</p>
+                  <p>Area: {area}</p>
+            </div>
+      );
+};
+
+export default Country;
