5a344387655d46b0246af280ba87632973beb304
Ashikmridha793 <ashikmridha793@gmail.com>
2025-12-05 00:49:49 +0600

    added new section

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
+import { Linter } from "eslint";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/independent?status=true")
@@ -11,13 +13,26 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const hendleVisitedCountry = (country) =>{
+    console.log('add to your visited country')
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  } 
+
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
-
+      <div>
+        <h4>Visited Countries: {visitedCountries.length}</h4>
+        <ul>
+          {
+            visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+          }
+        </ul>
+      </div>
       <div className="country-container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country key={country.cca3} hendleVisitedCountry={hendleVisitedCountry} country={country}></Country>
         ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, hendleVisitedCountry }) => {
       const {name, flags, population, area, cca3} = country
       const [visited, setVisited] = useState(false);
       const hendleVisited = () => {
             setVisited(!visited)
       }
+      
       return (
-            <div className="country">
-                  <h3>{name.common}</h3>
+            <div className={`country ${visited  ? 'visited' : 'non-visited'}`}>
+                  <h3 style={{color: visited ? 'purple' : 'white'}}>{name.common}</h3>
                   <img src={flags.png} alt="" />
                   <p>Total Population: {population}</p>
                   <p>Area: {area}</p>
                   <p><small>Code: {cca3}</small></p>
+                  <button onClick={() =>hendleVisitedCountry(country)}>Mark visited</button> <br />
                   <button onClick={hendleVisited}>{visited ? 'Visited':'Visit now'}</button><br />
                   {visited ? 'I have visited this country': 'I want to visit'}
             </div>
