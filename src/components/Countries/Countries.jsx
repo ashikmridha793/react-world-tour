99ed72d99cffd720132e61f0805cb16ef8fa3165
Ashikmridha793 <ashikmridha793@gmail.com>
2025-12-05 23:35:47 +0600

    added flags section

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
-import { Linter } from "eslint";
+// import { Linter } from "eslint";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
   const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlag] = useState([])
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/independent?status=true")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  const handleVisitedFlags = flag => {
+    const newVisitedFlags = [...visitedFlags, flag]
+    setVisitedFlag(newVisitedFlags);
+  }
 
   const hendleVisitedCountry = (country) =>{
     console.log('add to your visited country')
@@ -22,6 +28,7 @@ const Countries = () => {
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
+      {/* visited country */}
       <div>
         <h4>Visited Countries: {visitedCountries.length}</h4>
         <ul>
@@ -30,9 +37,19 @@ const Countries = () => {
           }
         </ul>
       </div>
+      <div className="flag-container">
+          {
+            visitedFlags.map(flag => <img src={flag}></img>)
+          }
+      </div>
+      {/* display country */}
       <div className="country-container">
         {countries.map((country) => (
-          <Country key={country.cca3} hendleVisitedCountry={hendleVisitedCountry} country={country}></Country>
+          <Country 
+          key={country.cca3} 
+          hendleVisitedCountry={hendleVisitedCountry} 
+          handleVisitedFlags={handleVisitedFlags}
+          country={country}></Country>
         ))}
       </div>
     </div>
